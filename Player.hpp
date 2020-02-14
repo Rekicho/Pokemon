c946008a17838694cf4680ac1a21d4b28f8ecca2
Rekicho <up201604145@fe.up.pt>
2020-02-14 11:51:59 +0000

    Player orientation

--- Player.hpp
+++ Player.hpp
@@ -3,15 +3,94 @@
 
 #include "Pokedex.hpp"
 #include "Pokemon.hpp"
+#include <SFML/Graphics.hpp>
 #include <vector>
+
+enum Orientation
+{
+    UP,
+    LEFT,
+    RIGHT,
+    DOWN
+};
 
 class Player
 {
     const Name name;
     std::vector<Pokemon> party;
+    sf::Texture textures[3];
+    sf::Sprite sprite;
+    sf::Vector2f position;
+    Orientation orientation;
 
 public:
-    Player(Name name) : name(name) {}
+    Player(Name name) : name(name), textures{sf::Texture(), sf::Texture(), sf::Texture()}, orientation(Orientation::UP)
+    {
+        sf::Image playerImage;
+        playerImage.loadFromFile("../images/people.png");
+        playerImage.createMaskFromColor(sf::Color(0, 255, 0));
+
+        textures[0].loadFromImage(playerImage, sf::IntRect(112, 269, 16, 16));
+        textures[1].loadFromImage(playerImage, sf::IntRect(131, 269, 16, 16));
+        textures[2].loadFromImage(playerImage, sf::IntRect(6, 288, 16, 16));
+
+        position = sf::Vector2f(8.f,8.f);
+        sprite.setOrigin(8.f,8.f);
+    }
+
+    void draw(sf::RenderWindow &window)
+    {
+        switch (orientation)
+        {
+        case Orientation::UP:
+            sprite.setScale(1.f, 1.f);
+            sprite.setTexture(textures[1]);
+            break;
+        case Orientation::LEFT:
+            sprite.setScale(1.f, 1.f);
+            sprite.setTexture(textures[2]);
+            break;
+        case Orientation::DOWN:
+            sprite.setScale(1.f, 1.f);
+            sprite.setTexture(textures[0]);
+            break;
+        case Orientation::RIGHT:
+            sprite.setScale(-1.f, 1.f);
+            sprite.setTexture(textures[2]);
+            break;
+        }
+
+        sprite.setPosition(position);
+        window.draw(sprite);
+    }
+
+    void handleInput(sf::Keyboard::Key key)
+    {
+        switch (key)
+        {
+        case sf::Keyboard::W:
+            //move(sf::Vector2f(0, -16));
+            orientation = Orientation::UP;
+            break;
+        case sf::Keyboard::A:
+            //move(sf::Vector2f(-16, 0));
+            orientation = Orientation::LEFT;
+            break;
+        case sf::Keyboard::S:
+            //move(sf::Vector2f(0, 16));
+            orientation = Orientation::DOWN;
+            break;
+        case sf::Keyboard::D:
+            //move(sf::Vector2f(16, 0));
+            orientation = Orientation::RIGHT;
+            break;
+        }
+    }
+
+    void move(sf::Vector2f move)
+    {
+        position += move;
+    }
 };
 
 #endif
--- Pokemon.cpp
+++ Pokemon.cpp
@@ -1,9 +1,13 @@
 #include <SFML/Graphics.hpp>
 
 #include <iostream>
+#include <cstdlib>
+#include <time.h>
 
-static const unsigned int windowWidth = 800;
-static const unsigned int windowHeigth = 600;
+#include "Player.hpp"
+
+static const unsigned int windowWidth = 640;
+static const unsigned int windowHeigth = 640;
 
 sf::Texture grassTexture;
 sf::Sprite grassSprite;
@@ -14,34 +18,57 @@ void load()
     grassSprite.setTexture(grassTexture);
 }
 
+void randomEncounter()
+{
+    if (rand() % 10 < 1)
+    {
+        if (rand() % 20 < 9)
+        {
+            std::cout << "Rattata" << std::endl;
+        }
+
+        else
+            std::cout << "Pidgey" << std::endl;
+    }
+}
+
 void draw(sf::RenderWindow &window)
 {
-    sf::Vector2u dimensions = grassTexture.getSize();
-    
-    for (int j = 0; j <= windowHeigth - dimensions.y; j += dimensions.y)
-        for (int i = ((j / dimensions.y) % 2) * dimensions.x; i <= windowWidth - dimensions.x; i += 2 * dimensions.x)
-        {
-            grassSprite.setPosition(sf::Vector2f((float)i, (float)j));
-            window.draw(grassSprite);
-        }
+    // sf::Vector2u dimensions = grassTexture.getSize();
+
+    // for (int j = 0; j <= windowHeigth - dimensions.y; j += dimensions.y)
+    //     for (int i = 0; i <= windowWidth - dimensions.x; i += dimensions.x)
+    //     {
+    //         grassSprite.setPosition(sf::Vector2f((float)i, (float)j));
+    //         window.draw(grassSprite);
+    //     }
 }
 
 int main()
 {
+    srand(time(NULL));
     load();
     sf::RenderWindow window(sf::VideoMode(windowWidth, windowHeigth), "Pokemon");
+    Player player("Red");
 
     while (window.isOpen())
     {
         sf::Event event;
         while (window.pollEvent(event))
         {
-            if (event.type == sf::Event::Closed)
+            switch (event.type)
+            {
+            case sf::Event::Closed:
                 window.close();
+                break;
+            case sf::Event::KeyReleased:
+                player.handleInput(event.key.code);
+            }
         }
 
         window.clear(sf::Color::Black);
         draw(window);
+        player.draw(window);
 
         window.display();
     }
